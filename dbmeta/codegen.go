87fe8d870f38dbb783d6ae9999dbb483ec278f66
Genesis <bsomsak.dev@gmail.com>
2024-09-29 21:22:08 +0700

    test: rename module name

--- dbmeta/codegen.go
+++ dbmeta/codegen.go
@@ -8,7 +8,7 @@ import (
 	"path"
 	"time"
 
-	"github.com/smallnest/gen/utils"
+	"github.com/athomeplatform/gendomain/utils"
 
 	"go/format"
 	"io/ioutil"
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/smallnest/gen
+module github.com/athomeplatform/gendomain
 
 go 1.16
 
